1334b67949b07f64a57bd56daf9a9474b1129d74
Habeebah Muse <hmuse@hmuse-ltm6cpu.internal.salesforce.com>
2023-07-19 14:36:00 -0700

    completed token, generating token

--- the-lamb-project-api/middleware/security.js
+++ the-lamb-project-api/middleware/security.js
@@ -4,21 +4,25 @@ const { UnauthorizedError } = require("../utils/errors");
 
 const jwtForm = ({ headers }) => {
   if (headers?.authorization) {
-    const [scheme, token] = headers.authorization.split;
-    (" ");
+    const [scheme, token] = headers.authorization.split(" ");
     if (scheme.trim() === "Bearer") {
-      console.log("cleaned token", scheme, token);
+      console.log("cleaning token", scheme, token);
+      return token;
     }
   }
   return undefined;
 };
+
 const extractUserFromJwt = (req, res, next) => {
+  console.log("extracting jwt");
   try {
     const token = jwtForm(req);
+    console.log("token", token);
     if (token) {
       res.locals.user = jwt.verify(token, SECRET_KEY);
       console.log("res.local", res.locals.user);
     }
+    return next();
   } catch (err) {
     return next();
   }
