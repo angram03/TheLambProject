6cc7354763d8c5a687658c00d34d542489443c16
Habeebah Muse <hmuse@hmuse-ltm6cpu.internal.salesforce.com>
2023-07-27 14:23:12 -0700

    fixing

--- the-lamb-project-ui/src/Components/MatchedCityPage/MatchedCityPage.jsx
+++ the-lamb-project-ui/src/Components/MatchedCityPage/MatchedCityPage.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import "./MatchedCityPage.css";
 import MatchedCityCard from "../MatchedCityCard/MatchedCityCard";
 // import "../../Icons/accept.png";
-const MatchedCityPage = ({ AcceptCity }) => {
+const MatchedCityPage = ({ AcceptCity, swiped, outOfFrame, lastDirection }) => {
   // const RejectCity = (event) => {
   //   console.log("Reject");
   // };
+
   return (
     <div>
-      <MatchedCityCard AcceptCity={AcceptCity} />
+      <MatchedCityCard
+        AcceptCity={AcceptCity}
+        swiped={swiped}
+        outOfFrame={outOfFrame}
+        lastDirection={lastDirection}
+      />
     </div>
   );
 };
