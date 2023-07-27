70729e94ec1e8073479f810581eec1fd8ea09400
Habeebah Muse <hmuse@hmuse-ltm6cpu.internal.salesforce.com>
2023-07-27 14:18:30 -0700

    app

--- the-lamb-project-ui/src/App.jsx
+++ the-lamb-project-ui/src/App.jsx
@@ -21,13 +21,39 @@ import MatchedCityListPage from "./Components/MatchedCityPage/MatchedCityPage";
 const App = () => {
   const [appState, setAppState] = useState({});
   const [likedCityCount, setLikedCityCount] = useState(0);
+  const [number, setNumber] = useState(1);
+  const [lastDirection, setLastDirection] = useState();
+  const [cities, setCities] = useState([]);
+  const [allCities, setAllCities] = useState([]);
 
   const AcceptCity = (event) => {
     setLikedCityCount(likedCityCount + 1);
     console.log(likedCityCount);
     console.log("Accept");
   };
+  const swiped = (direction, nameToDelete) => {
+    setAllCities([...allCities, nameToDelete]);
+    console.log("allCitites", allCities);
+    console.log("removing:" + nameToDelete);
 
+    if (direction == "right") {
+      setNumber(number + 1);
+      setCities([...cities, nameToDelete]);
+    }
+    if (direction == "down" || direction == "up") {
+      restoreCard();
+      preventSwipe(direction);
+      //so I have the city name right, I want to try to put it back where it was,
+      // how do I control that, when I swipe, how does the slide get out of the page.
+    }
+    console.log("Number", number);
+
+    setLastDirection(direction);
+  };
+  console.log("Cities", cities);
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen");
+  };
   return (
     <div>
       <BrowserRouter>
