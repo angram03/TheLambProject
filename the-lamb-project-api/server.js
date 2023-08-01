3450f6406be0b5630d9df69055aef679be72c7d2
Habeebah Muse <hmuse@hmuse-ltm6cpu.internal.salesforce.com>
2023-08-01 10:14:02 -0700

    tokenfixed

--- the-lamb-project-api/server.js
+++ the-lamb-project-api/server.js
@@ -14,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
+app.use(security.extractUserFromJwt);
+
 app.use("/auth", authRoutes);
 app.use("/user", userPreferenceRoutes);
 
