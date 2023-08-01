d354196debd125bd13dd9b07c70359e03dd26ee2
Habeebah Muse <hmuse@hmuse-ltm6cpu.internal.salesforce.com>
2023-08-01 10:25:05 -0700

    fixing token

--- the-lamb-project-api/server.js
+++ the-lamb-project-api/server.js
@@ -19,6 +19,28 @@ app.use(security.extractUserFromJwt);
 
 app.use("/auth", authRoutes);
 app.use("/user", userPreferenceRoutes);
+app.use("/api", weatherRoute);
+
+app.use((req, res, next) => {
+  console.log("404: Route not found.");
+  return next(new NotFoundError());
+});
+
+app.use((req, res, next) => {
+  return next(new NotFoundError());
+});
+
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message;
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
 
 app.get("/", function (req, res) {
   console.log("HELLO");
@@ -28,21 +50,23 @@ app.get("/", function (req, res) {
 });
 
 // Add the new route handler here
-// app.get('/api/city/:cityName/scores', async (req, res, next) => {
-//   const cityName = req.params.cityName;
+app.get("/api/city/:cityName/scores", async (req, res, next) => {
+  const cityName = req.params.cityName;
 
-//   try {
-//     const response = await axios.get(`https://api.teleport.org/api/urban_areas/slug:${cityName}/scores/`);
-//     const scores = response.data;
-//     res.json(scores);
-//   } catch (error) {
-//     if (error.response && error.response.status === 404) {
-//       return next(new NotFoundError('City scores not found on Teleport API.'));
-//     }
-//     console.error('Error fetching scores:', error.message);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
+  try {
+    const response = await axios.get(
+      `https://api.teleport.org/api/urban_areas/slug:${cityName}/scores/`
+    );
+    const scores = response.data;
+    res.json(scores);
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      return next(new NotFoundError("City scores not found on Teleport API."));
+    }
+    console.error("Error fetching scores:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
