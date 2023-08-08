e46d8b4c5a77fbb53ca5870a6e9c73bdad8c8496
Lucia Barboza <100799812+luciabarboza@users.noreply.github.com>
2023-08-08 10:15:55 -0700

    changed city score url

--- the-lamb-project-ui/src/Components/CityScores/CityScores.jsx
+++ the-lamb-project-ui/src/Components/CityScores/CityScores.jsx
@@ -8,7 +8,7 @@ const CityScores = ({ city }) => {
   async function getCityScore() {
     try {
       const response = await axios.get(
-        `http://localhost:3001/api/city/${city}/scores`
+        `https://lifetracker-api-w918.onrender.com/api/city/${city}/scores`
       );
       const responseData = response.data;
       setCityName(responseData._links?.ua?.name || "");
